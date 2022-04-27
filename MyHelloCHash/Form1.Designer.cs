7f376f234ac33c47a0d2ff671dbf9611faf17f77
SooooMm <soooomm00@gmail.com>
2022-04-27 10:00:43 +0900

    #2 환영 인사 버튼 추가 btn_Gretting 으로 해줬다

--- MyHelloCHash/Form1.Designer.cs
+++ MyHelloCHash/Form1.Designer.cs
@@ -28,13 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_Greeting = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_Greeting
+            // 
+            this.btn_Greeting.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btn_Greeting.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Greeting.Font = new System.Drawing.Font("굴림", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_Greeting.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Greeting.Location = new System.Drawing.Point(39, 23);
+            this.btn_Greeting.Name = "btn_Greeting";
+            this.btn_Greeting.Size = new System.Drawing.Size(583, 378);
+            this.btn_Greeting.TabIndex = 0;
+            this.btn_Greeting.Text = "Welcome~";
+            this.btn_Greeting.UseVisualStyleBackColor = false;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 269);
+            this.ClientSize = new System.Drawing.Size(645, 434);
+            this.Controls.Add(this.btn_Greeting);
             this.Name = "FormMain";
             this.Text = "MyHelloCHash";
             this.ResumeLayout(false);
@@ -42,6 +57,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Greeting;
     }
 }
 
